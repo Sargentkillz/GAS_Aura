55e8ce3eac21d27e10857232726d3133128548dd
coty lindblom <fullback12140202@gmail.com>
2024-04-13 13:39:18 -0400

    PostProcess Highligh

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -3,12 +3,23 @@
 
 #include "Character/AuraEnemy.h"
 
+AAuraEnemy::AAuraEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+}
+
 void AAuraEnemy::HighlightActor()
 {
-	
+	GetMesh()->SetCustomDepthStencilValue(250);
+	Weapon->SetCustomDepthStencilValue(250);
+	GetMesh()->SetRenderCustomDepth(true);
+	Weapon->SetRenderCustomDepth(true);
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
-	
+	GetMesh()->SetCustomDepthStencilValue(0);
+	Weapon->SetCustomDepthStencilValue(0);
+	GetMesh()->SetRenderCustomDepth(false);
+	Weapon->SetRenderCustomDepth(false);
 }
